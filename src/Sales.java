0322a99527062fe91e85885b447bb65d0e6fae67
c1027525Newcastle <c1027525@newcastle.ac.uk>
2022-02-16 13:56:56 +0000

    Made the constructor, getters and setters for the sales class.

--- src/Sales.java
+++ src/Sales.java
@@ -1,6 +1,48 @@
 public class Sales {
     //House no, postcode, value, month and year the house was sold
+    private int houseNumber;
+    private String postCode;
+    private int value;
+    private String date;
 
     //constructor to store and retrieve
 
+    public Sales(int houseNumber, String postCode, int value, String date) {
+        this.houseNumber = houseNumber;
+        this.postCode = postCode;
+        this.value = value;
+        this.date = date;
+    }
+
+    public int getHouseNumber(){
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber){
+        this.houseNumber = houseNumber;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
