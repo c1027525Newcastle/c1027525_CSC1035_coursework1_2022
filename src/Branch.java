613d574ac31ff08ef4788ad7f04a724a007cb687
c1027525Newcastle <c1027525@newcastle.ac.uk>
2022-02-24 21:03:07 +0000

    Made the constructor and elements of the Branch class and managed to allow the user to enter the data for the branch.

--- src/Branch.java
+++ src/Branch.java
@@ -1,5 +1,20 @@
+import java.util.List;
+
 public class Branch {
     // name of local branch, list of sales that it has had
+    public String name;
+    public List salesObject;
+
+    public Branch(String name, List salesObject) {
+        this.name = name;
+        this.salesObject = salesObject;
+    }
+
+    public String toStringBranch(){
+        String result = "";
+        result = "\nBranch name: " + name + "\nDetails: " + salesObject;
+        return result;
+    }
 
     // return: the sale with the highest value, the average sale value recorded by the branch in a given year,
     // return:a list of all sales with a value greater than a given amount of money
--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         int number;
         String postCode;
         int value;
         int year;
         String month;
+        String branchName;
 
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.println("Enter the house num");
+        System.out.println("Enter the house num as an integer");
         number = keyboard.nextInt();
 
         System.out.println("Enter the postcode");
         postCode = keyboard.next();
 
-        System.out.println("Enter the value");
+        System.out.println("Enter the value as an integer");
         value = keyboard.nextInt();
 
-        System.out.println("Enter the year");
+        System.out.println("Enter the year as an integer");
         year = keyboard.nextInt();
 
         System.out.println("Enter the month");
         month = keyboard.next();
 
+        // Create the object in the class and then check if it's written correctly
         Sales object1 = new Sales(number, postCode, value, year, month);
         System.out.println(object1.toString());
+
+        System.out.println("Enter the name of the branch");
+        branchName = keyboard.next();
+
+        List salesList = new ArrayList<Sales>();
+        salesList.add(object1);
+
+        // Create the object in the class and then check if it's written correctly
+        Branch branch1 = new Branch(branchName, salesList);
+        System.out.println(branch1.toStringBranch());
+
     }
 }
